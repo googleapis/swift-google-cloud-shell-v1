31100daf068b7a07fde33421fbc68035e0159199
Carlos O'Ryan <coryan@google.com>
2026-09-21 20:42:28 -0400

    chore: update librarian w/ better enum docs (#1017)
    In this version of librarian we improve the generated documentation for
    `enum` so the possible evolution and usage of the enums is more obvious.
    For example, we recommend using `@unknown default:` so they get warnings
    when new values appear in the enum.

--- Sources/GoogleCloudShellV1/StartEnvironmentMetadata.swift
+++ Sources/GoogleCloudShellV1/StartEnvironmentMetadata.swift
@@ -84,6 +84,12 @@ public struct StartEnvironmentMetadata: Codable, Equatable, GoogleWKT._AnyPackab
   /// show a progress message to the user. An environment won't necessarily go
   /// through all of these states when starting. More states are likely to be
   /// added in the future.
+  ///
+  /// - Note: Adding cases to this enumeration is not considered a breaking change.
+  ///   Always include an `@unknown default:` case when switching over this type.
+  ///   Do not pattern-match against `unknownStringValue` or `unknownIntValue`
+  ///   expecting specific values to remain unparsed; future releases may promote
+  ///   them to named cases.
   public enum State: Codable, Equatable, Sendable {
     /// The environment's start state is unknown.
     case unspecified
@@ -105,15 +111,21 @@ public struct StartEnvironmentMetadata: Codable, Equatable, GoogleWKT._AnyPackab
     case awaitingComputeResources
     /// Encodes an unknown integer value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific integer values in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownIntValue(Int)
     /// Encodes an unknown string value.
     ///
-    /// The most common cause for an unknown values is for the service to send
+    /// The most common cause for an unknown value is for the service to send
     /// a value unknown to the library. We recommend you update your library to
     /// the latest version.
+    ///
+    /// - Warning: Do not pattern-match specific string literals in this case;
+    ///   future releases may promote them to named enum cases.
     case unknownStringValue(String)
 
     public init() {
